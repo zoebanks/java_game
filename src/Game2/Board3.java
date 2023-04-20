a2f8bd59d833361d7f682d8f6ae60ae07a7bcc87
antoineTauvelENSEA <84904367+antoineTauvelENSEA@users.noreply.github.com>
2023-04-20 16:34:44 +0200

    A little improvment in waldo's game

---
+++ src/Game2/Board3.java
@@ -0,0 +1,62 @@
+package Game2;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.util.Random;
+
+public class Board3 extends Application {
+
+    private static final int numberOfGuys=300;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Random rd = new Random();
+        primaryStage.setTitle("Game 2 - Board");
+        Group root = new Group();
+        Image people = new Image("file:./img/blue_button_small.png");
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setScene(scene);
+        ImageView[] peopleView= new ImageView[numberOfGuys];
+        for (int i=0;i<numberOfGuys;i++){
+            peopleView[i]=new ImageView(people);
+            peopleView[i].setX(rd.nextInt(700));
+            peopleView[i].setY(rd.nextInt(700));
+            double scale=rd.nextDouble()+0.5;
+            peopleView[i].setScaleX(scale);
+            peopleView[i].setScaleY(scale);
+
+            root.getChildren().add(peopleView[i]);
+        }
+        Image waldo = new Image("file:./img/red_button_small.png");
+        ImageView waldoView = new ImageView(waldo);
+        waldoView.setX(rd.nextInt(700));
+        waldoView.setY(rd.nextInt(700));
+        double scale=rd.nextDouble()+0.5;
+        waldoView.setScaleX(scale);
+        waldoView.setScaleY(scale);
+        root.getChildren().add(waldoView);
+        primaryStage.show();
+        waldoView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Got me !");
+            }
+        });
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
